973ea0af4ee72c2c9535c951fd6766c9074bc5ea
Mariana <maryhsiano@hotmail.com>
2023-04-20 15:13:53 -0300

    'AVL.cpp' terminado

--- Arvore-AVL/AVL.cpp
+++ Arvore-AVL/AVL.cpp
@@ -63,3 +63,151 @@ NoAVL *AVL::rotacaoDireitaEsquerda(NoAVL *no) {
     no->setDir(rotacaoDireita(no->getDir()));
     return rotacaoEsquerda(no);
 }
+
+NoAVL *AVL::inserir(NoAVL *no, int chave) {
+    if (no == NULL) {
+        return new NoAVL(chave);
+    }
+
+    if (chave < no->getChave()) {
+        no->setEsq(inserir(no->getEsq(), chave));
+    }
+
+    else if (chave > no->getChave()) {
+        no->setDir(inserir(no->getDir(), chave));
+    }
+
+    else {
+        return no;
+    }
+
+    no->setAltura(1 + max(altura(no->getEsq()), altura(no->getDir())));
+
+    int balanceamento = fatorBalanceamento(no);
+
+    if (balanceamento > 1 && chave < no->getEsq()->getChave()) {
+        return rotacaoDireita(no);
+    }
+
+    if (balanceamento < -1 && chave > no->getDir()->getChave()) {
+        return rotacaoEsquerda(no);
+    }
+
+    if (balanceamento > 1 && chave > no->getEsq()->getChave()) {
+        return rotacaoEsquerdaDireita(no);
+    }
+
+    if (balanceamento < -1 && chave < no->getDir()->getChave()) {
+        return rotacaoDireitaEsquerda(no);
+    }
+
+    return no;
+}
+
+NoAVL *AVL::remover(NoAVL *no, int chave)
+{
+    if (no == NULL) {
+        return no;
+    }
+
+    if (chave < no->getChave()) {
+        no->setEsq(remover(no->getEsq(), chave));
+    }
+
+    else if (chave > no->getChave()) {
+        no->setDir(remover(no->getDir(), chave));
+    }
+
+    else {
+        if ((no->getEsq() == NULL) || (no->getDir() == NULL)) {
+            NoAVL *temp = no->getEsq() ? no->getEsq() : no->getDir();
+
+            if (temp == NULL) {
+                temp = no;
+                no = NULL;
+            }
+
+            else {
+                *no = *temp;
+            }
+
+            free(temp);
+        }
+
+        else {
+            NoAVL *temp = minimo(no->getDir());
+
+            no->setChave(temp->getChave());
+
+            no->setDir(remover(no->getDir(), temp->getChave()));
+        }
+    }
+
+    if (no == NULL) {
+        return no;
+    }
+
+    no->setAltura(1 + max(altura(no->getEsq()), altura(no->getDir())));
+
+    int balanceamento = fatorBalanceamento(no);
+
+    if (balanceamento > 1 && fatorBalanceamento(no->getEsq()) >= 0) {
+        return rotacaoDireita(no);
+    }
+
+    if (balanceamento > 1 && fatorBalanceamento(no->getEsq()) < 0) {
+        return rotacaoEsquerdaDireita(no);
+    }
+
+    if (balanceamento < -1 && fatorBalanceamento(no->getDir()) <= 0) {
+        return rotacaoEsquerda(no);
+    }
+
+    if (balanceamento < -1 && fatorBalanceamento(no->getDir()) > 0) {
+        return rotacaoDireitaEsquerda(no);
+    }
+
+    return no;
+}
+
+NoAVL *AVL::minimo(NoAVL *no) {
+    NoAVL *atual = no;
+
+    while (atual->getEsq() != NULL) {
+        atual = atual->getEsq();
+    }
+
+    return atual;
+}
+
+void AVL::preOrdem(NoAVL *no) {
+    if (no != NULL) {
+        cout << no->getChave() << " ";
+        preOrdem(no->getEsq());
+        preOrdem(no->getDir());
+    }
+}
+
+void AVL::emOrdem(NoAVL *no) {
+    if (no != NULL) {
+        emOrdem(no->getEsq());
+        cout << no->getChave() << " ";
+        emOrdem(no->getDir());
+    }
+}
+
+void AVL::posOrdem(NoAVL *no) {
+    if (no != NULL) {
+        posOrdem(no->getEsq());
+        posOrdem(no->getDir());
+        cout << no->getChave() << " ";
+    }
+}
+
+void AVL::limpar(NoAVL *no) {
+    if (no != NULL) {
+        limpar(no->getEsq());
+        limpar(no->getDir());
+        delete no;
+    }
+}
