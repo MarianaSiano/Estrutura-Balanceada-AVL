9476c85521b0f4d939bc3f6d3d163ea5ef20af5f
Mariana <maryhsiano@hotmail.com>
2023-04-20 14:38:29 -0300

    Class avl concluida

--- Arvore-AVL/NoAVL.h
+++ Arvore-AVL/NoAVL.h
@@ -9,4 +9,16 @@ private:
     int altura;
     NoAVL *esq;
     NoAVL *dir;
+
+public:
+    NoAVL(int chave);
+    ~NoAVL();
+    int getChave();
+    int getAltura();
+    NoAVL *getEsq();
+    NoAVL *getDir();
+    void setChave(int chave);
+    void setAltura(int altura);
+    void setEsq(NoAVL *esq);
+    void setDir(NoAVL *dir);
 };
