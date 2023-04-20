f2567598c0b9538986ba44803acafe13dde1bfb5
Mariana <maryhsiano@hotmail.com>
2023-04-20 18:21:00 -0300

    Refazendo o .cpp do no

--- NoAVL.cpp
+++ NoAVL.cpp
@@ -8,53 +8,13 @@ using namespace std;
 NoAVL::NoAVL(int chave)
 {
     this->chave = chave;
-    this->altura = 0;
-    this->esq = NULL;
-    this->dir = NULL;
+    this->altura = 1;
+    this->esquerda = NULL;
+    this->direita = NULL;
 }
 
 NoAVL::~NoAVL()
 {
-    delete this->esq;
-    delete this->dir;
-}
-
-int NoAVL::getChave()
-{
-    return this->chave;
-}
-
-int NoAVL::getAltura()
-{
-    return this->altura;
-}
-
-NoAVL *NoAVL::getEsq()
-{
-    return this->esq;
-}
-
-NoAVL *NoAVL::getDir()
-{
-    return this->dir;
-}
-
-void NoAVL::setChave(int chave)
-{
-    this->chave = chave;
-}
-
-void NoAVL::setAltura(int altura)
-{
-    this->altura = altura;
-}
-
-void NoAVL::setEsq(NoAVL *esq)
-{
-    this->esq = esq;
-}
-
-void NoAVL::setDir(NoAVL *dir)
-{
-    this->dir = dir;
+    delete this->esquerda;
+    delete this->direita;
 }
