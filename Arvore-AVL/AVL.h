32a49f5d9822831cd28c877770e62938b2e37d5f
Mariana <maryhsiano@hotmail.com>
2023-04-20 14:41:22 -0300

    'AVL.h' pronto

--- Arvore-AVL/AVL.h
+++ Arvore-AVL/AVL.h
@@ -1,4 +1,25 @@
 #include <bits/stdc++.h>
 #include <iostream>
 
+#include "NoAVL.h"
+
 using namespace std;
+
+class AVL {
+private:
+    NoAVL *raiz;
+
+    int altura(NoAVL *no);
+    int fatorBalanceamento(NoAVL *no);
+    NoAVL *rotacaoEsquerda(NoAVL *no);
+    NoAVL *rotacaoDireita(NoAVL *no);
+    NoAVL *rotacaoEsquerdaDireita(NoAVL *no);
+    NoAVL *rotacaoDireitaEsquerda(NoAVL *no);
+    NoAVL *inserir(NoAVL *no, int chave);
+    NoAVL *remover(NoAVL *no, int chave);
+    NoAVL *minimo(NoAVL *no);
+    void preOrdem(NoAVL *no);
+    void emOrdem(NoAVL *no);
+    void posOrdem(NoAVL *no);
+    void limpar(NoAVL *no);
+};
